571a16b229add29d6b7b4a01557b31431667223e
sabagh1994 <sabaghaffari@gmail.com>
2025-06-17 19:05:37 -0500

    accessibility epi_analysis added

---
+++ recent_preprocessing/pipeline/epi_analysis/BedToolsDiffExp_ACC_noOverlap/final/xpct_run.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#mkdir -p narrowPeak/10pct
+#./xpct.sh narrowPeak/all/DiffAcc.narrowPeak 0.1 narrowPeak/10pct/DiffAcc.narrowPeak
+
+#mkdir -p narrowPeak/30pct
+#./xpct.sh narrowPeak/all/DiffAcc.narrowPeak 0.3 narrowPeak/30pct/DiffAcc.narrowPeak
+
+#mkdir -p narrowPeak/40pct
+#./xpct.sh narrowPeak/all/DiffAcc.narrowPeak 0.4 narrowPeak/40pct/DiffAcc.narrowPeak
+
+mkdir -p narrowPeak/20pct
+./xpct.sh narrowPeak/all/DiffAcc.narrowPeak 0.2 narrowPeak/20pct/DiffAcc.narrowPeak
